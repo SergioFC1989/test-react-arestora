8824232dda5e0c317d3293161772329e89df2770
SergioFC1989 <sergiofc@hotmail.es>
2024-09-13 19:26:40 +0200

    fix: fix sintax reducer

--- src/reducers/threadReducer.js
+++ src/reducers/threadReducer.js
@@ -2,8 +2,8 @@ import { threadAction } from "../actions/threadAction";
 
 export const threadReducer = (state = {}, action) => {
   switch (action.type) {
-    case threadAction.FETCH_THREAD_REQUEST ||
-      threadAction.ACCEPT_THREAD_REQUEST:
+    case threadAction.FETCH_THREAD_REQUEST:
+    case threadAction.ACCEPT_THREAD_REQUEST:
       return { ...state, loading: true, error: null };
 
     case threadAction.FETCH_THREAD_SUCCESS:
@@ -12,8 +12,8 @@ export const threadReducer = (state = {}, action) => {
     case threadAction.ACCEPT_THREAD_SUCCESS:
       return { ...state, loading: false, thread: action.payload };
 
-    case threadAction.FETCH_THREAD_FAILURE ||
-      threadAction.ACCEPT_THREAD_FAILURE:
+    case threadAction.FETCH_THREAD_FAILURE:
+    case threadAction.ACCEPT_THREAD_FAILURE:
       return { ...state, loading: false, error: action.payload };
 
     default:
